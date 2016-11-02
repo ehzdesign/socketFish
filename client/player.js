deb8e54a1fde24aaaa2dba264c0b90fa6387c5ce
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2016-11-01 21:44:13 -0400

    countdown html added

--- client/player.js
+++ client/player.js
@@ -2,7 +2,7 @@
 var socket = io();
 
 //store player name that is given through input
-var playerInputName = $('#player-name');
+var playerInputName = $('#username');
 
 // create a player object
 var player = {};
@@ -12,7 +12,9 @@ var player = {};
 $('#join-btn').on('click', function(event) {
   event.preventDefault();
 
+  console.log(playerInputName.length);
 
+  if(playerInputName.val()){
 
   // // store the player name in player object
   player.name = playerInputName.val();
@@ -20,48 +22,82 @@ $('#join-btn').on('click', function(event) {
   //store a unique socket id for player
   player.id = socket.id;
 
+  //player type is equal to fish
+  player.type = 'fish';
+
   //send player object to server
   socket.emit('new-player', player);
 
-  //clear the register form
-  $('.register-player').html('');
+  console.log(player);
 
-  $('#message').text('waiting for more players, be patient');
+  loadPregame();
 
-});
+  //clear the register form ajax load next page
+  // $('.register-player').html('');
 
+  // $('#message').text('waiting for more players, be patient');
 
-
-socket.on('players', function(msg){
-  console.log(msg);
-  displayCurrentPlayers(msg);
-});
-
-
-
-socket.on('players-set', function(msg) {
-  $('#message').text('we have enough players, lets start the game!');
-  //clear the register form
-  $('.register-player').html('');
-  //show the players currently playing
-  displayCurrentPlayers(msg);
-  setTimeout(redirectToGame(), 1000);
+}
 
 });
 
 
-//show all current players logged in
-function displayCurrentPlayers(players) {
-  // clear the players list
-  $('#players').html('');
-  //print all the players names and id's
-  $.each(players, function(index, el) {
-    $('<li>').text('name:' + el.name + ' id:' + el.id).appendTo($('#players'));
+function loadPregame(){
+  $.ajax({
+    url: 'template/pregame.html',
+  })
+  .done(function(response) {
+    console.log("success");
+
+    if ($("#main").html() != response) {
+    $("#content").fadeOut(200, function() {
+      $("#main").html(response);
+      $('.countdown').fadeIn().delay(1000);
+    });
+    // $('#main').html(result);
+
+  }
+})
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
   });
-}
+};
 
 
-//redirect to game pager
-function redirectToGame() {
-  window.location.href = 'game.html';
-}
+
+// socket.on('players', function(msg){
+//   console.log(msg);
+//   displayCurrentPlayers(msg);
+// });
+//
+//
+//
+// socket.on('players-set', function(msg) {
+//   $('#message').text('we have enough players, lets start the game!');
+//   //clear the register form
+//   $('.register-player').html('');
+//   //show the players currently playing
+//   displayCurrentPlayers(msg);
+//   setTimeout(redirectToGame(), 1000);
+//
+// });
+
+//
+// //show all current players logged in
+// function displayCurrentPlayers(players) {
+//   // clear the players list
+//   $('#players').html('');
+//   //print all the players names and id's
+//   $.each(players, function(index, el) {
+//     $('<li>').text('name:' + el.name + ' id:' + el.id).appendTo($('#players'));
+//   });
+// }
+//
+//
+// //redirect to game pager
+// function redirectToGame() {
+//   window.location.href = 'game.html';
+// }
